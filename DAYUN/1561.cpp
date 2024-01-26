32d970ba90a245be5b8ab4b86359e89bc6fbc605
dy1110 <jeondayoon27@gmail.com>
2024-01-26 18:03:29 +0900

    1561

--- DAYUN/1561.cpp
+++ DAYUN/1561.cpp
@@ -6,17 +6,14 @@ int play[10000];
 
 int main() {
     //n : child m : play
-    int n, m;
-    int mmin = 30, mmax = 0;
-    //cin >> n >> m;
-    n = 2000000000;
-    m = 1;
+    long long n, m;
+    long long mmin = 0;
+    long long mmax = (long long)30 * (long long)2000000000;
+    cin >> n >> m;
     for (int i = 0; i < m; i++)
     {
-        //cin >> play[i];
-        play[i] = 1;
-        if (mmin > play[i]) mmin = play[i];
-        if (mmax < play[i]) mmax = play[i];
+        cin >> play[i];
+
     }
 
     // binary search
@@ -27,30 +24,30 @@ int main() {
     }
     n = n - m;
 
-    mmax *= n;
-    int mid = 0;
+    //mmax *= n;
+    long long mid = 0;
+    long long t = 0;
     while (mmin < mmax) {
-        //cout << mmin <<" " << mmax << endl;
-        //mid = (mmin + mmax) / 2;
-        mid = mmin / 2 + mmax / 2;
-        int nChild = 0;
+        mid = (mmin + mmax) / (long long)2;
+        long long nChild = 0;
         for (int i = 0; i < m; i++)
         {
-            nChild += (mid / play[i]);
+            nChild += (long long)(mid / play[i]);
         }
 
-        if (nChild > n) mmax = mid - 1;
+        if (nChild >= n) {
+            mmax = mid - 1;
+            t = mid;
+        }
         else if (nChild < n) {
             mmin = mid + 1;
-        }
-        if(nChild == n) {
-            break;
         }
     }
 
     //find min n
     //cout << mid << " " << mmin << " " << mmax << endl;
-
+    // t가 딱 n명째 태우는 정확한 시간
+    // t-1일 때 몇명 태우는지? 그 뒤로 운행시간 mod 연산이 0이 될때마다 1씩추가
 
     int nChild = n;
     while (nChild == n) {
@@ -76,11 +73,10 @@ int main() {
     }
     cout << k + 1;
 
-
-
 }
 
-
+//딱 1초 전만 확인하면 된다.
+//long long으로 변환
 
 
 //최소공배수
