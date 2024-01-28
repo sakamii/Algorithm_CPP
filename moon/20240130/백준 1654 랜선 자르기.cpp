222235cca592164d3a239640fbdb6c845674c533
sejinmoon <156970461+sejinmoon@users.noreply.github.com>
2024-01-28 22:10:25 +0900

    [백준] 1654 랜선 자르기

--- moon/20240130/백준 1654 랜선 자르기.cpp
+++ moon/20240130/백준 1654 랜선 자르기.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 vector<int> v;
@@ -11,19 +10,25 @@ int max_m = -1;
 int m;
 int result;
 
+// binary search 
 void bs(long int low, long int high) {
 	int sum = 0;
 	long int mid = (low + high) / 2;
 
+	// 종료 조건
 	if (low > high) return;
 
+	// 랜선 갯수 구하기
 	for (int i = 0; i < v.size(); i++) {
 		sum += v[i] / mid;
 	}
+
+	// 랜선 갯수가 넘친다면 길이 늘려서 조사
 	if (sum >= k) {
 		if (max_m < mid) max_m = mid;
 		bs(mid + 1, high);
 	}
+	// 랜선 갯수가 부족하다면 길이 줄여 조사
 	else {
 		bs(low, mid - 1);
 	}
@@ -45,7 +50,7 @@ int main() {
 
 
 	bs(1, max_v);
-	
+
 	cout << max_m;
 
 
