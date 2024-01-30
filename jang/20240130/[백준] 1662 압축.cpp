8267429ccb324f7a0c24ad8be22c6d20b6bb7ef0
yoonseok99 <109502705+yoonseok99@users.noreply.github.com>
2024-01-30 17:23:49 +0900

    Update [백준] 1662 압축.cpp

--- jang/20240130/[백준] 1662 압축.cpp
+++ jang/20240130/[백준] 1662 압축.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <string>
 using namespace std;
 
 int main() {
@@ -10,7 +11,7 @@ int main() {
 	//char로 넣으면 일의 자리수가 아닌 수를 다시 push 하게 될 때 문제가 됨
 	//따라서 int로 설정(아스키코드로)
 	for (int i = 0; i < a.length(); i++) {
-		if (a[i] == ')') {
+		if (a[i] == ')') { //닫히는 괄호가 나오기 전까지 push. 그 전에 무조건 열리는 괄호 있음
 			int temp = 0;
 			while (!A.empty()) {
 				int top1 = A.top();
@@ -22,7 +23,7 @@ int main() {
 				if (top1 == '(') break;
 				else temp++;
 			}
-			if (A.empty()) continue;
+			if (A.empty()) continue; //처음 부분이 ()...로 시작하는 경우
 			else {
 				int top1 = (int)A.top()-'0';
 				A.pop();
@@ -32,7 +33,8 @@ int main() {
 				else if (top1 == '(') answer = temp;
 				else answer = temp * top1;
 			}
-			if(answer!=0) A.push(answer+1000);
+			if(answer!=0) A.push(answer+1000); 
+			//1000을 넘어가는 아스키코드는 없다고 판단,18번줄에서 구분하기 위해 1000을 더한다.
 		}
 		else A.push(a[i]);
 	}
