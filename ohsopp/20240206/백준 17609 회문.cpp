b964efdd84b4d9e3bf0059cf5c696700aeffd72a
ohsopp <wjdgus0545@naver.com>
2024-02-05 22:11:27 +0900

    [백준] 17609 회문

---
+++ ohsopp/20240206/백준 17609 회문.cpp
@@ -0,0 +1,33 @@
+
+// https://www.acmicpc.net/problem/17609
+
+#include <iostream>
+using namespace std;
+
+int n;
+string s;
+
+int dfs(int l, int r, int c, bool flag) {
+	if (l > r) return min(c, 2);
+
+	int ret = 2;
+	if (s[l] == s[r])
+    return dfs(l + 1, r - 1, c, flag);
+	else {
+		if (!flag) {
+				ret = dfs(l + 1, r, c + 1, 1);
+				ret = min(ret, dfs(l, r - 1, c + 1, 1));
+		}
+		else ret = 2;
+		return ret;
+	}
+}
+
+int main() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		cout << dfs(0, s.size() - 1, 0, 0) << endl;
+	}
+}
