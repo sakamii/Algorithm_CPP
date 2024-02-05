f77b3c07ee723e608586ab0e0ed47d5427e33a43
ohsopp <wjdgus0545@naver.com>
2024-02-05 22:36:07 +0900

    [백준] 17609 회문

--- ohsopp/20240206/백준 17609 회문.cpp
+++ ohsopp/20240206/백준 17609 회문.cpp
@@ -8,17 +8,17 @@ int n;
 string s;
 
 int dfs(int l, int r, int c, bool flag) {
-	if (l > r) return min(c, 2);
+	if (l > r) return min(c, 2);	// c: 문자열에서 s[l]과 s[r]이 달랐던 횟수
 
 	int ret = 2;
-	if (s[l] == s[r]) 
+	if (s[l] == s[r]) // 양쪽 문자가 같으면 양쪽 한 글자씩 줄이면서 dfs 진행
 		return dfs(l + 1, r - 1, c, flag);
-	else {
-		if (!flag) {
-				ret = dfs(l + 1, r, c + 1, 1);
-				ret = min(ret, dfs(l, r - 1, c + 1, 1));
+	else {	// 양쪽 문자가 다를 때
+		if (!flag) {	// 다른 적이 처음이라면
+				ret = dfs(l + 1, r, c + 1, 1);	// 왼쪽 줄여서 진행한 것과
+				ret = min(ret, dfs(l, r - 1, c + 1, 1)); // 오른쪽 줄여서 진행한 것중 작은 값으로
 		}
-		else ret = 2;
+		else ret = 2;	// 이미 전적이 있다? 바로 2 반환
 		return ret;
 	}
 }
