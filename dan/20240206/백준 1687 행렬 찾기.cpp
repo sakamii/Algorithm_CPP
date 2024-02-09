22fbdb1662b3ae45e93d4daff79c494bce8a3958
Yudan Lee <125230094+sakamii@users.noreply.github.com>
2024-02-09 18:34:26 +0900

    [백준] 1687 행렬 찾기

--- dan/20240206/백준 1687 행렬 찾기.cpp
+++ dan/20240206/백준 1687 행렬 찾기.cpp
@@ -1,46 +1,32 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <stack>
 using namespace std;
 
+int n, m, ans, arr[335][335];
+char ch;
+
 int main() {
-	string arr[334];
-	int n, m, max_mn = 0;
 	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	int prefixsum[335][335] = { 0 };
-	// 초기값 설정;
-	for (int i = 0; i < n; i++) {
-		prefixsum[i+1][1] = (arr[i][0]-'0');
-	}
-	// 행 방향 합 구하기
-	for (int i = 0; i < n; i++) {
-		for (int j = 1; j < m; j++) {
-			prefixsum[i+1][j+1] = prefixsum[i+1][j] + (arr[i][j]-'0');
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++) {
+			cin >> ch;
+			arr[i][j] = ch == '0';
+			if (arr[i][j]) arr[i][j] += arr[i - 1][j];
 		}
-	}
-	// 열 방향 합 구하기
-	for (int i = 0; i < m; i++) {
-		for (int j = 1; j < n; j++) {
-			prefixsum[j+1][i+1] = prefixsum[j][i+1] + prefixsum[j+1][i+1];
-		}
-	}
-	// 여기서 시간초과 뜸 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			for (int y = 1; y < n+1; y++) {
-				for (int x = 1; x < m+1; x++) {
-					if (y <= i || x <= j) continue;
-					int max_value = prefixsum[y][x] - prefixsum[y][j] - prefixsum[i][x] + prefixsum[i][j];
-					if (max_value == 0) {
-						if ((y - i) * (x - j) > max_mn) max_mn = (y - i) * (x - j);
-						int de = -1;
-					}
-				}
+
+	for (int i = 1; i <= n; i++) {
+		stack<int> st;
+		st.push(0);
+
+		for (int j = 1; j <= m + 1; j++) {
+			while (!st.empty() && arr[i][st.top()] > arr[i][j]) {
+				int idx = st.top();
+				st.pop();
+				ans = max(ans, arr[i][idx] * (j - st.top() - 1));
 			}
+			st.push(j);
 		}
 	}
-	cout << max_mn;
+	cout << ans;
 }
