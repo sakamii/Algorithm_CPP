b04f6460a50e7d861ed1377689878b6a03a75af4
Yudan Lee <125230094+sakamii@users.noreply.github.com>
2024-02-09 16:12:53 +0900

    [백준] 1561 놀이공원

--- dan/20240130/백준 1561 놀이공원.cpp
+++ dan/20240130/백준 1561 놀이공원.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 using namespace std;
-int m, n, t[100001], max_value = 0, answer=0;
-// 시간을 딱 받고 나서 애들이 모두 타고 나왔는지 확인하고 딱 n명이 타고 나온 시간이 나오면
-// 이때 멈추고 얘로 나눠 떨어지는 애들 중에 가장 큰 수를 answer로 내보냄
+int n, m, mid, t, l = 1, r = 60000000000;
+vector<int>v;
 
 int main() {
 	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> t[i];
-		max_value += t[i];
+	for (int i = 0; i < m; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
 	}
-	int left = 0, right = max_value * ceil(n / m), mid = 0;
-	while (left <= right) {
-		mid = (left + right) / 2;
-		int mt = 0;
+	if (n <= m) {
+		cout << n;
+		return 0;
+	}
+	while (l <= r) {
+		mid = (l + r) / 2;
+		long long child = m;
 		for (int i = 0; i < m; i++) {
-			mt += mid / t[i];
+			child += mid / v[i];
 		}
-		if (mt > m) {
-			right = mid - 1;
-		}
-		else if (mt < m) {
-			left = mid + 1;
+		if (child >= n) {
+			t = mid;
+			r = mid - 1;
 		}
 		else {
-			break;
+			l = mid + 1;
 		}
 	}
+	long long people = m;
 	for (int i = 0; i < m; i++) {
-		if (mid % t[i] == 0)
-			answer = max(answer, t[i]);
+		people += (t - 1) / v[i];
 	}
-	cout << answer;
+
+	for (int i = 0; i < m; i++) {
+		if (!(t % v[i])) people++;
+		if (people == n) {
+			cout << i + 1;
+			return 0;
+		}
+	}
 }
