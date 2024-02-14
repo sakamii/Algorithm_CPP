bcb65984b8c0c1aa5be032ebd477f105202a4231
ohsopp <wjdgus0545@naver.com>
2024-02-14 21:12:26 +0900

    [SWEA] 1952 수영장

---
+++ ohsopp/20240220/SWEA 1952 수영장.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int t, a, b, c, d, m, dp[15];
+
+int main(){
+	cin >> t;
+	for (int tc = 1; tc <= t; tc++) {
+		cin >> a >> b >> c >> d;
+		for (int i = 3; i <= 14; i++) {
+			cin >> m;
+			dp[i] = min(dp[i - 1] + min(m * a, b), dp[i - 3] + c);
+		}
+    cout << "#" << tc << " " << min(d, dp[14]) << '\n';
+	}
+}
