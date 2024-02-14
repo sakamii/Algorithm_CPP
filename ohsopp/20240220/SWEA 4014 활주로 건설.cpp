050ab04ad6d4a6cec9082fb9f7e963d7d4af9233
ohsopp <wjdgus0545@naver.com>
2024-02-15 00:00:23 +0900

    [SWEA] 4014 활주로 건설

--- ohsopp/20240220/SWEA 4014 활주로 건설.cpp
+++ ohsopp/20240220/SWEA 4014 활주로 건설.cpp
@@ -1,57 +1,53 @@
 
 #include <iostream>
 using namespace std;
-
-int t, n, x, ans, garo, sero, idx, arr[22][22];
-
+ 
+int t, n, x, ans, garo, sero, A[22][22], B[22][22];
+ 
+int calc(int i, int j, int arr[][22]) {
+    int ret = 1;
+ 
+    if (arr[i][j] == arr[i][j - 1] + 1) {   // 현재가 이전보다 1 클 때
+        int idx = j - 1;
+        while (arr[i][j - 1] == arr[i][idx]) idx--;
+        if (arr[i][idx] > arr[i][j - 1] && j - 1 - idx < 2 * x) ret = 0;
+        else if (j - 1 - idx < x) ret = 0;
+    }
+    else if (arr[i][j] + 1 == arr[i][j - 1]) {  // 현재가 이전보다 1 작을 때
+        int idx = j;
+        while (arr[i][j] == arr[i][idx]) idx++;
+        if (idx - j < x) ret = 0;
+    }
+    else if (arr[i][j] != arr[i][j - 1]) ret = 0;   // 차이가 2 이상 일 때
+ 
+    return ret;
+}
+ 
 int main() {
-	cin >> t;
-  
-	for (int tc = 1; tc <= t; tc++) {
-		cin >> n >> x;
-    
-		for (int i = 1; i <= n; i++)
-			for (int j = 1; j <= n; j++)
-				cin >> arr[i][j];
-
-		for (int i = 1; i <= n; i++) {
-			garo = 1, sero = 1;
-			for (int j = 2; j <= n; j++) {
-				if (!garo && !sero) break;
-
-				// 가로 -----------------------------------------------------
-				if (arr[i][j] == arr[i][j - 1] + 1) {	// 현재가 왼쪽보다 1 클 때
-					idx = j - 1;
-					while (arr[i][j - 1] == arr[i][idx]) idx--;
-					if (arr[i][idx] > arr[i][j - 1] && j - 1 - idx < 2 * x) garo = 0;
-					else if (j - 1 - idx < x) garo = 0;
-				}
-				else if (arr[i][j] + 1 == arr[i][j - 1]) {	// 현재가 왼쪽보다 1 작을 때
-					idx = j;
-					while (arr[i][j] == arr[i][idx]) idx++;
-					if (idx - j < x) garo = 0;
-				}
-				else if (arr[i][j] != arr[i][j - 1]) garo = 0;	// 차이가 2 이상 일 때
-
-				// 세로 -----------------------------------------------------
-				if (arr[j][i] == arr[j - 1][i] + 1) {	// 현재가 위쪽보다 1 클 때
-					idx = j - 1;
-					while (arr[j - 1][i] == arr[idx][i]) idx--;
-					if (arr[idx][i] > arr[j - 1][i] && j - 1 - idx < 2 * x) sero = 0;
-					else if (j - 1 - idx < x) sero = 0;
-				}
-				else if (arr[j][i] + 1 == arr[j - 1][i]) {	// 현재가 위쪽보다 1 작을 때
-					idx = j;
-					while (arr[j][i] == arr[idx][i]) idx++;
-					if (idx - j < x) sero = 0;
-				}
-				else if (arr[j][i] != arr[j - 1][i]) sero = 0;	// 차이가 2 이상 일 때
-			}
-			ans += garo + sero;
-		}
-
-		cout << "#" << tc << " " << ans << endl;
-		fill(arr[0], arr[22], 0);
-		ans = 0;
-	}
+    cin >> t;
+ 
+    for (int tc = 1; tc <= t; tc++) {
+        cin >> n >> x;
+ 
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++) {
+                cin >> A[i][j];
+                B[j][i] = A[i][j];
+            }
+ 
+        for (int i = 1; i <= n; i++) {
+            garo = sero = 1;
+            for (int j = 2; j <= n; j++) {
+                if (!garo && !sero) break;
+                if (garo) garo = calc(i, j, A); // 가로
+                if (sero) sero = calc(i, j, B); // 세로
+            }
+            ans += garo + sero;
+        }
+ 
+        cout << "#" << tc << " " << ans << endl;
+        fill(A[0], A[22], 0);
+        fill(B[0], B[22], 0);
+        ans = 0;
+    }
 }
