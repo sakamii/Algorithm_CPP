295913d7fd7dc5a7d86f4699b0c88f0e81fa7351
JeonDY <jeondayoon27@gmail.com>
2024-02-19 18:01:48 +0900

    2477

--- DAYUN/240220/2477.cpp
+++ DAYUN/240220/2477.cpp
@@ -1,10 +1,12 @@
 // SWEA 2477 차량 정비소
 
 /*
-고객 만적도 설문지 창구번호 접n수 정비m
+고객 만적도 설문지 창구번호 접수N 정비m
 각 시간(모든 다 동일) a b
 차량정비소 도착 시간 t
 빈 접수팡고
+
+
 
 */
 
@@ -56,12 +58,10 @@ int main(int argc, char **argv)
         for (int i = 0; i < N; i++)
         {
             cin >> a_times[i];
-            pqA.push({0, i});
         }
         for (int i = 0; i < M; i++)
         {
             cin >> b_times[i];
-            pqB.push({0, i});
         }
         for (int i = 0; i < K; i++)
         {
@@ -79,7 +79,8 @@ int main(int argc, char **argv)
                     a_rest[j] = t[i] + a_times[j];  
                     min_a_rest_j = -1;
                     pq_bt.push({a_rest[j], i});
-                    cout << j << " ";
+                    cout << a_rest[j] << " ";
+
                     resultA[i] = j;
                     break;
                 }
@@ -91,15 +92,16 @@ int main(int argc, char **argv)
             }
 
             if(min_a_rest_j != -1) {
-                a_rest[min_a_rest_j] += b_times[i];
+                a_rest[min_a_rest_j] += a_times[min_a_rest_j];
                 pq_bt.push({a_rest[min_a_rest_j], i});
+                cout << a_rest[min_a_rest_j] << " ";
                 resultA[i] = min_a_rest_j;
             }   
         }
-
-        cout << 1 <<endl;
+        // return 0;
+        cout << endl;
+        cout << "B" << endl;
         int b_rest[20] = {0};
-        cout << pq_bt.size(); 
         while(!pq_bt.empty())
         {
             // cout << 2;
@@ -107,32 +109,38 @@ int main(int argc, char **argv)
             int min_b_rest_j = 0;
             int time = pq_bt.top().time;
             int i = pq_bt.top().i;
+            cout << time << "," << i << " ";
             pq_bt.pop();
             for(int j = 0; j < M; j++) {
                 //사람이 온 시간 >= 창구가 비는 시간
                 if(time >= b_rest[j]) {
-                    b_rest[j] = time + b_times[i];  
+                    b_rest[j] = time + b_times[j];  
                     min_b_rest_j = -1;
+                    // cout << b_rest[j] << " ";
                     resultB[i] = j;
                     break;
                 }
 
                 if(b_rest[j] < min_b_rest_time) {
-                    min_b_rest_time = b_rest[i];
+                    min_b_rest_time = b_rest[j];
                     min_b_rest_j = j;
                 }
             }
 
             if(min_b_rest_j != -1) {
-                a_rest[min_b_rest_j] += b_times[i];
+                b_rest[min_b_rest_j] += b_times[min_b_rest_j];
+                // cout << b_rest[min_b_rest_j] << " ";
                 resultB[i] = min_b_rest_j;
             }   
+            // cout << resultB[i] << endl;
         }
 
         cout <<endl;
         for(int i = 0; i < K; i++) {
-            cout << resultA[i]  << ","<< resultB[i] << " ";
+            // cout << resultA[i]  << ","<< resultB[i] << " ";
             if((resultA[i] == A) && (resultB[i] == B)) {
+                cout << i << endl;
+
                 result += (i + 1);
             }
         }
