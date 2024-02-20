0b1fb9c54258a093ee99f77eb5d2866df47cba78
JeonDY <jeondayoon27@gmail.com>
2024-02-20 12:12:08 +0900

    [SWEA] 2477 차량 정비소

--- DAYUN/240220/SWEA 2477 차량 정비소.cpp
+++ DAYUN/240220/SWEA 2477 차량 정비소.cpp
@@ -1,10 +1,11 @@
 // SWEA 2477 차량 정비소
 
 /*
-고객 만적도 설문지 창구번호 접수N 정비m
-각 시간(모든 다 동일) a b
-차량정비소 도착 시간 t
-빈 접수팡고
+1. 접수 창고 모두 돌면서 pq_bt에 접수가 끝나는 시간, 접수한 사람, 접수 창고 번호(나중에 우선순위) 저장
+2. pq_bt에서 우선순위는 정비 창고 우선순위와 같다
+
+1.1 접수 창고를 도는 방법은 모든 번호순서대로 돌면서 사람이 시작한 시간t[i]가  끝나는 시간 보다  크면
+1.2 만약 모든 창고가 시작한 시간보다 작으면 가장 끝나는 시간에 접수한다. 
 */
 
 #include <iostream>
@@ -17,7 +18,10 @@ struct edge
     int time;
     int i;
     int j;
-    bool operator<(const edge &temp) const
+    //우선순위 : 정비 먼저 시작하는 조건
+    //1. 접수 끝난 순서 
+    //2. 접수 창고번호가 작은 수
+    bool operator<(const edge& temp) const
     {
         if (time > temp.time)
             return true;
@@ -28,27 +32,26 @@ struct edge
     };
 };
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    ``` int test_case;
+    int test_case;
     int T;
     cin >> T;
     for (test_case = 1; test_case <= T; ++test_case)
     {
-        // 빡구현
-        // input
         int N, M, K, A, B;
-        int a_times[20];
-        int b_times[20];
+        int a_times[20] = {0};
+        int b_times[20] = { 0 };
+        //접수가 끝나는 시간, 접수한 사람, 접수 창고 번호
         priority_queue<edge> pq_bt;
         int t[1000];
 
-        int resultA[20];
-        int resultB[20];
+        int resultA[1000] = { 0 };
+        int resultB[1000] = {0 };
         int result = 0;
 
         cin >> N >> M >> K >> A >> B;
@@ -67,9 +70,11 @@ int main(int argc, char **argv)
             cin >> t[i];
         }
 
-        int a_rest[20] = {0};
+        // 접수 창고가 끝나는 시간
+        int a_rest[20] = { 0 };
         for (int i = 0; i < K; i++)
         {
+            //가장 빨리 끝나는 창고 숫자, 시간
             int min_a_rest_time = 10000000;
             int min_a_rest_j = 0;
             for (int j = 0; j < N; j++)
@@ -79,9 +84,7 @@ int main(int argc, char **argv)
                 {
                     a_rest[j] = t[i] + a_times[j];
                     min_a_rest_j = -1;
-                    pq_bt.push({a_rest[j], i, j});
-                    cout << a_rest[j] << " ";
-
+                    pq_bt.push({ a_rest[j], i, j });
                     resultA[i] = j;
                     break;
                 }
@@ -93,26 +96,23 @@ int main(int argc, char **argv)
                 }
             }
 
+            //사람이 들어왔을 때 쉬는 창고가 없는 경우
             if (min_a_rest_j != -1)
             {
                 a_rest[min_a_rest_j] += a_times[min_a_rest_j];
-                pq_bt.push({a_rest[min_a_rest_j], i, min_a_rest_j});
-                cout << a_rest[min_a_rest_j] << " ";
+                pq_bt.push({ a_rest[min_a_rest_j], i, min_a_rest_j });
                 resultA[i] = min_a_rest_j;
             }
         }
-        // return 0;
-        cout << endl;
-        cout << "B" << endl;
-        int b_rest[20] = {0};
+
+        // 정비 창고가 끝나는 시간
+        int b_rest[20] = { 0 };
         while (!pq_bt.empty())
         {
-            // cout << 2;
             int min_b_rest_time = 10000000;
             int min_b_rest_j = 0;
             int time = pq_bt.top().time;
             int i = pq_bt.top().i;
-            cout << time << "," << i << " ";
             pq_bt.pop();
             for (int j = 0; j < M; j++)
             {
@@ -121,7 +121,6 @@ int main(int argc, char **argv)
                 {
                     b_rest[j] = time + b_times[j];
                     min_b_rest_j = -1;
-                    // cout << b_rest[j] << " ";
                     resultB[i] = j;
                     break;
                 }
@@ -136,20 +135,14 @@ int main(int argc, char **argv)
             if (min_b_rest_j != -1)
             {
                 b_rest[min_b_rest_j] += b_times[min_b_rest_j];
-                // cout << b_rest[min_b_rest_j] << " ";
                 resultB[i] = min_b_rest_j;
             }
-            // cout << resultB[i] << endl;
         }
 
-        cout << endl;
         for (int i = 0; i < K; i++)
         {
-            // cout << resultA[i]  << ","<< resultB[i] << " ";
             if ((resultA[i] == A) && (resultB[i] == B))
             {
-                cout << i << endl;
-
                 result += (i + 1);
             }
         }
@@ -158,11 +151,9 @@ int main(int argc, char **argv)
         {
             result = -1;
         }
-
         cout << "#" << test_case << " " << result << endl;
     }
 
     return 0;
-
-    ```
 }
+
