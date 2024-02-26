e7680273e1b3a258abf84658fb69f250c09d2b70
ohsopp <wjdgus0545@naver.com>
2024-02-26 22:52:33 +0900

    Create 백준 2294 동전2.cpp

---
+++ ohsopp/20240227/백준 2294 동전2.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int n, k, c, d[10010];
+
+int main(){
+    cin >> n >> k;
+    fill(d + 1, d + k + 1, 1e5);
+
+    while (n--) {
+        cin >> c;
+        for(int i = c; i <= k; i++)
+            d[i] = min(d[i], d[i - c] + 1);
+    }
+    cout << (d[k] < 1e5 ? d[k] : -1);
+}
