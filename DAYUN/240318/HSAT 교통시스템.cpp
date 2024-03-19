8919afc258fe3bf186ff4df4581b6ea1ddf47a20
JeonDY <jeondayoon27@gmail.com>
2024-03-19 17:11:34 +0900

    s

--- DAYUN/240318/HSAT 교통시스템.cpp
+++ DAYUN/240318/HSAT 교통시스템.cpp
@@ -1,55 +1,90 @@
-/* 
-    문제 이해 : 
-    n-분기  - n1
-    
-*/
-
-
-#include<iostream>
-#include<stack>
-#include<vector>
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int road[100][100][4];
-int visited[100][100];
-vector<pair<int, int>> signal[13] ={
-    {},
-    {{-1, 1}, {0, 1}, {1, 1}},
-    {{-1, -1}, {-1, 0}, {-1, 1}},
-    {{-1, -1}, {0, -1}, {1, -1}},
-    {{1, -1}, {1, 0}, {1, 1}},
-    {{-1, 1}, {0, 1}},
-    {{-1, -1}, {-1, 0}},
-    {{0, -1}, {1, -1}},
-    {{1, 0}, {1, 1}},
-    {{0, 1}, {1, 1}},
-    {{-1, 0}, {-1, 1}},
-    {{-1, -1}, {0, -1}},
-    {{1, -1}, {1, 0}}
+struct route
+{
+    int y;
+    int x;
+    // dir : 우 상 좌 하
+    int dir;
+    int ver;
+    int num_route;
+    int t;
 };
 
-int main() {
-    int n , t;
+int ydir[] = { 0, -1, 0, 1 };
+int xdir[] = { 1, 0, -1, 0 };
+
+int road[100][100][4];
+int routes[100];
+
+vector<int> signals[13] = {
+    {},
+    {1, 0, 3},
+    {2, 1, 0},
+    {1, 2, 3},
+    {2, 3, 0},
+    {0, 1},
+    {1, 2},
+    {2, 3},
+    {0, 3},
+    {0, 3},
+    {0, 1},
+    {1, 2},
+    {2, 3} };
+
+int result = 0;
+int n, t;
+
+void dfs(int y, int x, int dir, int num_route, int tt, int route);
+
+int main()
+{
     cin >> n >> t;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < t; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < t; j++)
+        {
             cin >> road[i][j][0] >> road[i][j][1] >> road[i][j][2] >> road[i][j][3];
         }
     }
 
-    int idx = 0;
-    stack<pair<int, int>> st;
-    //처음 진입
-    for(int i = 0; i < road[0][0][])
+    dfs(0, 0, 2, 1, 0, 0);
+    cout << result;
+}
 
-
-
-
-    while(!st.empty()) {
-        int y = st.top().first;
-        int x = st.top().second;
-
-
+void dfs(int y, int x, int dir, int num_route, int tt, int route)
+{
+    cout << tt << ":" << y << " " << x << endl;
+    if (road[y][x][t % 4] / 4 != dir || (tt == t)) {
+        result = max(result, num_route);
+        return;
     }
+
+    for (int signal : signals[road[y][x][t % 4]])
+    {
+        int nexty = y + ydir[signal];
+        int nextx = x + xdir[signal];
+        bool flag = false;
+        if ((nexty >= 0) && (nexty < n) && (nextx >= 0) && (nextx < n))
+        {
+            for (int i = 0; i < route; i++) {
+                if (routes[i] == (nexty * 100 + nextx)) {
+                    flag = true;
+                    break;
+                }
+            }
+            routes[route] = nexty * 100 + nextx;
+            if (flag) { dfs(nexty, nextx, signal, num_route, tt + 1, route + 1); }
+            else { dfs(nexty, nextx, signal, num_route, tt + 1, route + 1); }
+               
+        }
+    }
+
+    result = max(result, num_route);
+
 }
