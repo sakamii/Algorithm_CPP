77f23b21fb2c7ac24f00e228ac8e8b7c5b97a235
JeonDY <jeondayoon27@gmail.com>
2024-03-21 18:00:46 +0900

    Create 1.cpp

---
+++ DAYUN/240324/1.cpp
@@ -0,0 +1,93 @@
+/*
+    DNA 4종류 a c g t
+    염기서열 N개 길이 M 
+    와일드 카드 . (무엇이든 들어와도 좋다)
+    염기서열 조건을 만족할 수 있는 염기서열 : 초염기서열
+    초 염기서열을 만들어 여러 그룹의 연기서열 커버
+    최소 갯수의 초 염기서열?
+    모든
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int map[15][20];
+vector<pair<long long, long long>> range[15];
+bool visited[15] = {false};
+vector<vector<vector<int>>> v;
+int n, m;
+
+void dfs(int level);
+int min_result;
+
+int main(int argc, char** argv)
+{
+    cin >> n >> m;
+    v.resize(n);
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            char ch;
+            cin >> ch;
+            if(ch == 'a') map[i][j] = 0;
+            else if(ch == 'c') map[i][j] = 1;
+            else if(ch == 'g') map[i][j] = 2;
+            else if(ch == 't') map[i][j] = 3;
+            else map[i][j] = -1;
+        }
+    }
+
+    for(int i = 0; i < (1 << n); i++) {
+        bool flag = true;
+        int s[20];
+        memset(s, -1, sizeof(int) * m);
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < m; k++) {
+                if((i << j) & 1) {
+                    if(s[k] == -1 || map[j][k] == s[k]) {
+                        s[k] = map[j][k];
+                    }
+                    else {
+                        flag = false;
+                    }
+                }
+            }
+        }
+
+        if(flag) {
+            for(int j = 0; j < n; j++) {
+                if((i >> j) & 1){
+                    v[j].push_back(i);
+                }
+            }
+        }
+    }
+
+    dfs(0, 0);
+   return 0;
+}
+
+void dfs(int level, int result){
+    for(int i : v[level]) {
+        for(int j = level + 1; j < n; j++) {
+            if((i >> j) & 1) {
+                visited[i] = true;
+            }
+        }
+
+        bool flag = false;
+        for(int j = 0; j < n; j++) {
+            if(!visited[j]) {
+                dfs(level, result + 1);
+                flag = true;
+                break;
+            }
+        }
+        
+        if(!flag) {
+            min_result = min(result, min_result);
+        }
+    }
+
+}
