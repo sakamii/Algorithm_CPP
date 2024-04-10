fcd764d120ce04bc30f1fe26159db04e11f795c9
JeonDY <jeondayoon27@gmail.com>
2024-04-10 22:48:52 +0900

    Add files via upload

---
+++ DAYUN/240410/백준 11505 구간 곱 구하기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+const int SIZE = 1 << 21;
+int tree[SIZE];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+}
+
+
+/*
+    n, m , k
+    
+*/
