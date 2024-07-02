b0bdef04a0141b78d33df329934eee28f691c521
JeonDY <jeondayoon27@gmail.com>
2024-07-02 17:57:22 +0900

    2024.07.04

---
+++ DAYUN/240709/[MCD]관개 공사.cpp
@@ -0,0 +1,41 @@
+// 2024.07.04
+
+// 알고리즘의 탑 - MST의 탑 4번 
+// 최소 스패닝 트리 : 그래프의 최소 연결 부분 그래프
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct Node {
+    int node1;
+    int node2;
+    int value;
+
+    bool operator > const (const Node &temp) const {
+        return value > temp.value;
+    }
+};
+
+
+int N;
+int K;
+priority_queue<
+
+void init(){
+
+}
+
+
+int main() {
+    init();
+
+
+    
+
+    
+
+
+}
