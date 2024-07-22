573835c2d667f0b2ea4bd34f9058e25a0497b2cb
ohsopp <wjdgus0545@naver.com>
2024-07-22 21:16:34 +0900

    [백준] 1431 시리얼 번호

---
+++ ohsopp/20240723/백준 1431 시리얼 번호.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int asize, bsize, asum, bsum;
+string serial[51];
+
+int cmp(string a, string b) {
+	asize = a.size();
+	bsize = b.size();
+    asum = bsum = 0;
+
+	if (asize != bsize)
+		return asize < bsize;
+
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] >= '0' && a[i] <= '9')
+			asum += int(a[i]) - 48;
+
+		if (b[i] >= '0' && b[i] <= '9')
+			bsum += int(b[i]) - 48;
+	}
+
+	if (asum != bsum)
+		return asum < bsum;
+
+	return a < b;
+}
+
+int main() {
+	int N;
+	cin >> N;
+  
+	for (int i = 0; i < N; i++)
+		cin >> serial[i];
+
+	sort(serial, serial + N, cmp);
+	for (int i = 0; i < N; i++)
+		cout << serial[i] << "\n";
+}
